a3b58e80cd9f427ae01a3cd389eac9b7312f1ee0
Nate Wolfe <nwolfe@puppetlabs.com>
2015-02-28 13:23:17 -0800

    WIP

--- desktop/project.clj
+++ desktop/project.clj
@@ -1,6 +1,6 @@
 (defproject minesweeper "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  
+
   :dependencies [[com.badlogicgames.gdx/gdx "1.5.3"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.5.3"]
                  [com.badlogicgames.gdx/gdx-box2d "1.5.3"]
@@ -13,7 +13,7 @@
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
                  [play-clj "0.4.4"]]
-  
+
   :source-paths ["src" "src-common"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [minesweeper.core.desktop-launcher]
--- desktop/src-common/minesweeper/core.clj
+++ desktop/src-common/minesweeper/core.clj
@@ -11,13 +11,22 @@
     ;; (label "Hello world!" (color :white))
     (let [sheet (texture "minesweeper_tiles.jpg")
           tiles (texture! sheet :split 128 128)]
-      (texture (aget tiles 1 2)))
-    )
+      (texture (aget tiles 1 2))
+      ))
+
+  :on-wtf-man
+  (fn [s v])
 
   :on-render
   (fn [screen entities]
     (clear!)
-    (render! screen entities)))
+    (render! screen entities))
+
+  :on-touch-down
+  (fn [screen entities]
+    (println "x =" (game :x))
+    (println "y =" (game :y))
+    ))
 
 (defgame minesweeper
   :on-create
