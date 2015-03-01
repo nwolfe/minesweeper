7f9671b7d4cd227cfbe5aa39c50bf96c51b8ae9e
Nate Wolfe <a1979shakedown@gmail.com>
2015-02-28 16:48:52 -0800

    Create tile function

--- desktop/src-common/minesweeper/core.clj
+++ desktop/src-common/minesweeper/core.clj
@@ -1,7 +1,8 @@
 (ns minesweeper.core
   (:require [play-clj.core :refer :all]
             [play-clj.g2d :refer :all]
-            [play-clj.math :refer :all]))
+            [play-clj.math :refer :all]
+            [clojure.pprint :as pprint]))
 
 
 ;; Tiles 128x128
@@ -14,19 +15,38 @@
                       (rectangle! :contains (:x coords) (:y coords))))
                 entities)))
 
+(def tile-coordinates
+  {:block [0 0]
+   :flag  [0 1]
+   :bomb  [0 2]
+   :blank [0 3]
+   :one   [1 0]
+   :two   [1 1]
+   :three [1 2]
+   :four  [1 3]
+   :five  [2 0]
+   :six   [2 1]
+   :seven [2 2]
+   :eight [2 3]})
+
+(defn ->tile
+  [tile x y]
+  (let [[row col] (get tile-coordinates tile)]
+    (-> (texture "minesweeper_tiles.jpg")
+        (texture! :split 128 128)
+        (aget row col)
+        (texture)
+        (assoc :height 128 :width 128
+               :x x :y y))))
+
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage))
     ;; (label "Hello world!" (color :white))
-    (let [sheet (texture "minesweeper_tiles.jpg")
-          tiles (texture! sheet :split 128 128)]
-      (assoc (texture (aget tiles 1 2))
-             :height 128
-             :width 128
-             :x 0
-             :y 0)
-      ))
+    (let [flag (->tile :flag 0 0)]
+      [flag])
+    )
 
   :on-render
   (fn [screen entities]
@@ -36,7 +56,7 @@
   :on-touch-down
   (fn [screen entities]
     (let [target (get-entity-at-cursor screen entities)]
-      (clojure.pprint/pprint target)
+      (pprint/pprint target)
       )
     entities))
 
